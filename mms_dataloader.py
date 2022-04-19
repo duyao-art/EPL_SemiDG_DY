440d8b083b00c035e4bc188d5ae1ce8b83ff30d5
Your Name <you@example.com>
2022-04-20 01:06:25 +0800

    update

--- mms_dataloader.py
+++ mms_dataloader.py
@@ -720,7 +720,7 @@ class ImageFolder(data.Dataset):
         # pytorch: N,C,H,W
 
     def __len__(self):
-        return len(self.imgs) 
+        return len(self.imgs)
 
 
 if __name__ == '__main__':
