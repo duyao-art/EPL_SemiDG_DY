39ab3e58afe458c02f5a452a0d067a7cb437a841
Your Name <you@example.com>
2022-05-08 11:10:07 +0800

    update

--- config_u2pl_dy.py
+++ config_u2pl_dy.py
@@ -43,7 +43,8 @@ default_config = dict(
     ifFast=False,
     Pretrain=True,
     # pretrain_file='/home/duyao/my_data/duyao/MMData/resnet50_v1c.pth',
-    pretrain_file='/root/autodl-tmp/duyao/MMData/resnet50_v1c.pth',
+    pretrain_file='/home/qinzhang/qinggang/MMData/resnet50_v1c.pth',
+    # pretrain_file='/root/autodl-tmp/duyao/MMData/resnet50_v1c.pth',
 
     restore=False,
     restore_from=name+'.pt',
