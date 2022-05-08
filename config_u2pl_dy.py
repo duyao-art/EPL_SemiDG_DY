f7ba9a615d52b563f9dcfa1125652cba162710f1
Your Name <you@example.com>
2022-05-08 10:31:14 +0800

    update

--- config_u2pl_dy.py
+++ config_u2pl_dy.py
@@ -38,8 +38,8 @@ default_config = dict(
     # this parameter can be revised based on k-fold validation
     CPS_weight=3,
 
-    # gpus=[0,1],
-    gpus=[0],
+    gpus=[0,1],
+    # gpus=[0],
     ifFast=False,
     Pretrain=True,
     # pretrain_file='/home/duyao/my_data/duyao/MMData/resnet50_v1c.pth',
