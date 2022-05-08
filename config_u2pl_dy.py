a86d014e059d84d59e2af8661a946d3b9036d404
Your Name <you@example.com>
2022-05-08 11:26:30 +0800

    update

--- config_u2pl_dy.py
+++ config_u2pl_dy.py
@@ -29,8 +29,8 @@ default_config = dict(
     low_entropy_threshold=20,
     # num_negatives=50,
     # num_queries=256,
-    num_negatives=20,
-    num_queries=128,
+    num_negatives=10,
+    num_queries=64,
     temperature=0.5,
 
     train_name=name,
