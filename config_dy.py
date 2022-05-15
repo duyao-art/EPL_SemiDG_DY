4766b5ef12b195ff01571e2c4a2a4c8c8b93be65
Your Name <you@example.com>
2022-05-15 08:35:22 +0800

    update

--- config_dy.py
+++ config_dy.py
@@ -11,8 +11,8 @@ default_config = dict(
     alpha=0.75,
     # stop training to avoid overfitting
     # b=0.1588,
-    # b=0.1882,
-    b=0.2600,
+    b=0.1882,
+    # b=0.2600,
 
     num_class=4,
 
@@ -33,7 +33,7 @@ default_config = dict(
     # this parameter can be revised based on k-fold validation
     CPS_weight=3,
 
-    gpus=[6,7],
+    gpus=[5,6,7],
     ifFast=False,
     Pretrain=True,
     # pretrain_file='/home/duyao/my_data/duyao/MMData/resnet50_v1c.pth',
