5af9af10d0b5325750b159cb6766d1c1bad08fcc
Your Name <you@example.com>
2022-06-03 23:52:10 +0800

    update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 name = 'MMS_deeplab_2%_D_CM'
 # hyperparameter
 default_config = dict(
-    batch_size=28,
+    batch_size=8,
     num_epoch=50,
     learning_rate=1e-4,            # learning rate of Adam
     weight_decay=0.01,             # weight decay 
@@ -17,7 +17,7 @@ default_config = dict(
     # this parameter can be revised based on k-fold validation
     CPS_weight=3,
 
-    gpus=[4,5,6,7],
+    gpus=[7],
     ifFast=False,
     Pretrain=True,
     # pretrain_file='/home/duyao/my_data/duyao/MMData/resnet50_v1c.pth',
